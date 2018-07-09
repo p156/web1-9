15f0aa00c72e22d78c1e1773af754f31a455d8d7
Unknown <yuuichi714@yahoo.co.jp>
2018-07-09 18:29:47 +0900

    遅れたコミット

---
+++ AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // UIを使う時は忘れないように注意!!
+
+public class GameDirector : MonoBehaviour
+{
+
+    GameObject timerText;
+    GameObject pointText;
+    float time = 60.0f;
+    int point = 0;
+
+    public void GetApple()
+    {
+        this.point += 100;
+    }
+
+    public void GetBomb()
+    {
+        this.point /= 2;
+    }
+
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");
+    }
+
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + "point";
+    }
+}
